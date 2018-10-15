8d1fafc3e737aac76c50d9db79eaa16f5b1e94cb
Matt Ch. Rocha <mattchrocha@gmail.com>
2018-10-15 17:57:43 +0200

    heroku deploy

---
+++ server/public/precache-manifest.76f16849defc07337652f2f426093a68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0dcba378779514ccdc9a",
+    "url": "/static/js/main.0dcba378.chunk.js"
+  },
+  {
+    "revision": "d3ee19a3d8a2fdf76919",
+    "url": "/static/js/1.d3ee19a3.chunk.js"
+  },
+  {
+    "revision": "0dcba378779514ccdc9a",
+    "url": "/static/css/main.f44a6515.chunk.css"
+  },
+  {
+    "revision": "9a3eaa864f4457339e06d0bdae1a88aa",
+    "url": "/index.html"
+  }
+];
