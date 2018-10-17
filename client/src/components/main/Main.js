5324af261b7630fae1c0fbe5e5ab71918e9f40c3
Matt Ch. Rocha <mattchrocha@gmail.com>
2018-10-17 23:58:17 +0200

    estructura de home

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,29 +1,26 @@
-import React, {Component} from 'react';
-import './main.css';
-import Navfix from '../navfix/Navfix'
+import React, { Component } from "react";
+import "./main.css";
+import Navfix from "../navfix/Navfix";
 // import { CSSTransitionGroup } from 'react-transition-group'
-import TodoList from './TodoList'
-import Home from '../steps/Home'
-
-
+import TodoList from "./TodoList";
+import Home from "../steps/Home";
 
 export default class Main extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      style : {
+      style: {
         backgroundColor: "#BFACFD"
       }
-    }
+    };
   }
 
-
-  render(){
+  render() {
     return (
-      <div className="tap-main" style={{...this.state.style}}>
+      <div className="tap-main" style={{ ...this.state.style }}>
         <Navfix />
         <Home />
       </div>
-    )
+    );
   }
 }
